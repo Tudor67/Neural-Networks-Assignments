907864d516ab1ba5281e821be2cdae9d1eb5f1b7
Tudor Buzu <buzu.tudor67@gmail.com>
2018-10-09 22:21:18 +0300

    updated download scripts for cifar10 and mnist

--- datasets/download_cifar10.sh
+++ datasets/download_cifar10.sh
@@ -2,4 +2,4 @@
 wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
 tar -xzvf cifar-10-python.tar.gz
 rm cifar-10-python.tar.gz
-mv cifar-10-batches-py cifar10
+mv cifar-10-batches-py ../datasets/cifar10
--- datasets/download_mnist.sh
+++ datasets/download_mnist.sh
@@ -1,22 +1,22 @@
 # Get MNIST
-rm -rf mnist
-mkdir mnist
+rm -rf ../datasets/mnist
+mkdir ../datasets/mnist
 
 wget http://yann.lecun.com/exdb/mnist/index.html
-mv index.html ./mnist/readme.html
+mv index.html ../datasets/mnist/readme.html
 
 wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
 gzip -d train-images-idx3-ubyte.gz
-mv train-images-idx3-ubyte ./mnist/train-images
+mv train-images-idx3-ubyte ../datasets/mnist/train_images
 
 wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
 gzip -d train-labels-idx1-ubyte.gz 
-mv train-labels-idx1-ubyte ./mnist/train-labels
+mv train-labels-idx1-ubyte ../datasets/mnist/train_labels
 
 wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
 gzip -d t10k-images-idx3-ubyte.gz
-mv  t10k-images-idx3-ubyte ./mnist/test-images
+mv  t10k-images-idx3-ubyte ../datasets/mnist/test_images
 
 wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
 gzip -d t10k-labels-idx1-ubyte.gz
-mv t10k-labels-idx1-ubyte ./mnist/test-labels
+mv t10k-labels-idx1-ubyte ../datasets/mnist/test_labels
