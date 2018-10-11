a097a08f114168e98e6fc455f868728930544fb7
Tudor Buzu <buzu.tudor67@gmail.com>
2018-10-11 22:49:26 +0300

    added vis_utils

---
+++ utils/vis_utils.py
@@ -0,0 +1,56 @@
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.metrics import confusion_matrix
+
+
+def plot_train_val_loss_acc(train_loss, val_loss, train_acc, val_acc, title='sgd'):
+	plt.figure(figsize=(18, 6))
+
+	plt.subplot(1, 2, 1)
+	plt.plot(range(len(train_loss)), train_loss, label='train')
+	plt.plot(range(len(val_loss)), val_loss, label='val')
+	plt.legend()
+	plt.xlabel('epoch')
+	plt.ylabel('loss')
+	plt.title('loss [{}]'.format(title))
+
+	plt.subplot(1, 2, 2)
+	plt.plot(range(len(train_acc)), train_acc, label='train')
+	plt.plot(range(len(val_acc)), val_acc, label='val')
+	plt.legend()
+	plt.xlabel('epoch')
+	plt.ylabel('accuracy')
+	plt.title('accuracy [{}]'.format(title))
+
+
+def plot_confusion_matrix(y_pred, y, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    '''
+	From http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#confusion-matrix
+	'''
+    cm = confusion_matrix(y, y_pred).T
+    
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('Predicted label')
+    plt.xlabel('True label')
