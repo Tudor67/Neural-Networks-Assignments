12abab17d947825930d6333796c841944ba162d6
Tudor Buzu <buzu.tudor67@gmail.com>
2018-10-12 13:54:02 +0300

    small update in vis_utils

--- utils/vis_utils.py
+++ utils/vis_utils.py
@@ -6,28 +6,29 @@ from sklearn.metrics import confusion_matrix
 
 
 def plot_train_val_loss_acc(train_loss, val_loss, train_acc, val_acc, title='sgd'):
-	plt.figure(figsize=(18, 6))
+    plt.figure(figsize=(18, 6))
 
-	plt.subplot(1, 2, 1)
-	plt.plot(range(len(train_loss)), train_loss, label='train')
-	plt.plot(range(len(val_loss)), val_loss, label='val')
-	plt.legend()
-	plt.xlabel('epoch')
-	plt.ylabel('loss')
-	plt.title('loss [{}]'.format(title))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(len(train_loss)), train_loss, label='train')
+    plt.plot(range(len(val_loss)), val_loss, label='val')
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.title('loss [{}]'.format(title))
 
-	plt.subplot(1, 2, 2)
-	plt.plot(range(len(train_acc)), train_acc, label='train')
-	plt.plot(range(len(val_acc)), val_acc, label='val')
-	plt.legend()
-	plt.xlabel('epoch')
-	plt.ylabel('accuracy')
-	plt.title('accuracy [{}]'.format(title))
+    plt.subplot(1, 2, 2)
+    plt.plot(range(len(train_acc)), train_acc, label='train')
+    plt.plot(range(len(val_acc)), val_acc, label='val')
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('accuracy [{}]'.format(title))
 
 
 def plot_confusion_matrix(y_pred, y, classes,
                           normalize=False,
                           title='Confusion matrix',
+                          new_figure=False,
                           cmap=plt.cm.Blues):
     '''
     From http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#confusion-matrix
@@ -37,7 +38,9 @@ def plot_confusion_matrix(y_pred, y, classes,
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     
-    plt.figure(figsize=(8, 6))
+    if new_figure:
+        plt.figure(figsize=(8, 6))
+    
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
