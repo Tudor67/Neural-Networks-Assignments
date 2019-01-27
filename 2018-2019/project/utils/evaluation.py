0f5753a2f050450ea671940ac6491113fc6a7daa
Tudor Buzu <buzu.tudor67@gmail.com>
2019-01-27 23:38:13 +0200

    added tf implementation for jaccard and dice coefficients

--- 2018-2019/project/utils/evaluation.py
+++ 2018-2019/project/utils/evaluation.py
@@ -1,4 +1,6 @@
+import config
 import numpy as np
+import tensorflow as tf
 
 def get_tp_fp_fn(a, b):
     a = np.equal(a, 1)
@@ -17,7 +19,7 @@ def jaccard(a, b):
     
     jaccard_coef = None
     if tp + fp + fn == 0:
-        jaccard_coef = 1
+        jaccard_coef = 1.
     else:
         jaccard_coef = tp / (tp + fp + fn)
         
@@ -28,8 +30,43 @@ def dice(a, b):
     
     dice_coef = None
     if tp + fp + fn == 0:
-        dice_coef = 1
+        dice_coef = 1.
     else:
         dice_coef = (2 * tp) / (2 * tp + fp + fn)
     
+    return dice_coef
+
+# tensorflow implementation (with thr)
+def tf_get_tp_fp_fn(a_in, b_in):
+    a = tf.greater_equal(a_in, config.PRED_THR)
+    not_a = tf.logical_not(a)
+    b = tf.greater_equal(b_in, config.PRED_THR)
+    not_b = tf.logical_not(b)
+    
+    tp_and = tf.logical_and(a, b)
+    tp_count = tf.count_nonzero(tp_and)
+    tp = tf.cast(tp_count, tf.float64)
+    
+    fp_and = tf.logical_and(a, not_b)
+    fp_count = tf.count_nonzero(fp_and)
+    fp = tf.cast(fp_count, tf.float64)
+    
+    fn_and = tf.logical_and(a, b)
+    fn_count = tf.count_nonzero(fn_and)
+    fn = tf.cast(fn_count, tf.float64)
+    
+    return tp, fp, fn
+
+def tf_jaccard(a, b):
+    tp, fp, fn = tf_get_tp_fp_fn(a, b)
+    jaccard_coef = tf.cond(tf.equal(tp + fp + fn, 0),
+                           lambda: tf.constant(1, tf.float64),
+                           lambda: tp / (tp + fp + fn))
+    return jaccard_coef
+
+def tf_dice(a, b):
+    tp, fp, fn = tf_get_tp_fp_fn(a, b)
+    dice_coef = tf.cond(tf.equal(tp + fp + fn, 0),
+                        lambda: tf.constant(1, tf.float64),
+                        lambda: (2 * tp) / (2 * tp + fp + fn))
     return dice_coef
