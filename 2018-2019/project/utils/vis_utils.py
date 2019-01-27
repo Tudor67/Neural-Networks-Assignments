0aba1a0562b4733f1a9403446be2712ae5b9457a
Tudor Buzu <buzu.tudor67@gmail.com>
2019-01-27 23:36:45 +0200

    small updates in vis_utils.py

--- 2018-2019/project/utils/vis_utils.py
+++ 2018-2019/project/utils/vis_utils.py
@@ -42,29 +42,43 @@ def grid_vis_for_crop_and_merge(img, img_patches, mask, mask_patches):
             plt.axis('off')
             plt.imshow(mask_patches[i][j], cmap='gray')
             
-def plot_image_mask_prediction(images, masks, preds, dataset_split=''):
+def plot_image_mask_prediction(images, masks, preds, dataset_split='', pred_thr=None):
     print(f'dataset_split: {dataset_split}')
+    
+    num_cols = 4
+    fig_size = (10, 2)
+    if pred_thr is not None:
+        num_cols = 5
+        fig_size = (12, 2)
+        
     for idx in range(len(preds)):
-        plt.figure(figsize=(10, 2))
-        plt.subplot(1, 4, 1)
+        plt.figure(figsize=fig_size)
+        plt.subplot(1, num_cols, 1)
         plt.title(f'initial image ({dataset_split})')
         plt.axis('off')
         plt.imshow(images[idx])
-        plt.subplot(1, 4, 2)
+        plt.subplot(1, num_cols, 2)
         plt.title('ground truth')
         plt.axis('off')
         plt.imshow(masks[idx].squeeze(), cmap='gray')
-        plt.subplot(1, 4, 3)
+        plt.subplot(1, num_cols, 3)
         plt.title('prediction')
         plt.axis('off')
         plt.imshow(preds[idx].squeeze(), cmap='gray')
-        plt.subplot(1, 4, 4)
-        plt.title('values of the prediction')
+        plt.subplot(1, num_cols, 4)
+        plt.title('prediction values')
         plt.axis('on')
         plt.hist(preds[idx].flatten(), density=True)
+        
+        if pred_thr is not None:
+            plt.subplot(1, num_cols, 5)
+            plt.title(f'prediction (thr={pred_thr})')
+            plt.axis('off')
+            plt.imshow(preds[idx].squeeze() > pred_thr, cmap='gray')
+        
         plt.show()
         
-def plot_train_val(train, val, fig_title='Loss', y_label='binary_cross_entropy'):
+def plot_train_val(train, val, fig_title='Loss', y_label='binary cross entropy'):
     x = 1 + np.arange(len(train))
     plt.figure(figsize=(8, 5))
     plt.title(f'{fig_title} evolution')
