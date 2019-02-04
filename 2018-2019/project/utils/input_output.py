5a15bc75523e9aa764b74d7a3092239f0966310c
Tudor Buzu <buzu.tudor67@gmail.com>
2019-02-04 22:37:08 +0200

    added rename_images_with_uniform_ind_len in input_output.py

--- 2018-2019/project/utils/input_output.py
+++ 2018-2019/project/utils/input_output.py
@@ -25,3 +25,23 @@ def tiff_to_png(in_path, out_path):
         img = skimage.io.imread(f'{in_path}/{img_name}')
         img_name_png = '.'.join(img_name.split('.')[:-1]) + '.png'
         skimage.io.imsave(f'{out_path}/{img_name_png}', img)
+        
+def rename_images_with_uniform_ind_len(dir_path):
+    img_names = sorted(os.listdir(dir_path))
+
+    for img_name in img_names:
+        img_name_pref = '_'.join(img_name.split('_')[:-2])
+        img_format = img_name.split('.')[-1]
+
+        i = img_name.split('_')[-2]
+        if len(i) < 2:
+            i = '0' + i
+
+        j = img_name.split('_')[-1].split('.')[0]
+        if len(j) < 2:
+            j = '0' + j
+
+        new_img_name = f'{img_name_pref}_{i}_{j}.{img_format}'
+
+        os.rename(f'{dir_path}/{img_name}',
+                  f'{dir_path}/{new_img_name}')
