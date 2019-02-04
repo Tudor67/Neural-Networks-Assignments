330b7969ac610dc61c17a8dd7bea3ac9787fa09b
Tudor Buzu <buzu.tudor67@gmail.com>
2019-02-04 03:00:31 +0200

    added tiff_to_png for inria_aerial dataset

--- 2018-2019/project/utils/input_output.py
+++ 2018-2019/project/utils/input_output.py
@@ -1,5 +1,6 @@
 import json
 import os
+import skimage
 
 def read_json(filename):
     with open(filename, 'r') as fi:
@@ -13,3 +14,14 @@ def write_json(data, filename):
         
     with open(filename, 'w') as fo:
         json.dump(data, fo)
+        
+def tiff_to_png(in_path, out_path):
+    if not os.path.isdir(out_path):
+        os.makedirs(out_path)
+    
+    img_names = os.listdir(in_path)
+    
+    for img_name in img_names:
+        img = skimage.io.imread(f'{in_path}/{img_name}')
+        img_name_png = '.'.join(img_name.split('.')[:-1]) + '.png'
+        skimage.io.imsave(f'{out_path}/{img_name_png}', img)
