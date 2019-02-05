2a42f125a474eabe28da9e2209c3bcd3de15bb81
Tudor Buzu <buzu.tudor67@gmail.com>
2019-02-05 12:41:29 +0200

    trained unet_sal_map_guided on inria_aerial/subset_chicago

--- 2018-2019/project/config.py
+++ 2018-2019/project/config.py
@@ -1,14 +1,14 @@
-#BACKPROP_MODIFIER = 'guided'
-#UNET_NAME = f'UNet_SAL_MAP_{BACKPROP_MODIFIER.upper()}'
-UNET_NAME = f'UNet_RGB'
+BACKPROP_MODIFIER = 'guided'
+UNET_NAME = f'UNet_SAL_MAP_{BACKPROP_MODIFIER.upper()}'
+#UNET_NAME = f'UNet_RGB'
 #DATASET_NAME = 'icoseg/subset_80'
 DATASET_NAME = 'inria_aerial/subset_chicago'
 EXPERIMENT_NAME = f'{DATASET_NAME}/{UNET_NAME}'
 
 PRED_THR = 0.5
-#INPUT = 'sal_map'
-INPUT = 'rgb'
-INPUT_SHAPE = (512, 512, 3)
+INPUT = 'sal_map'
+#INPUT = 'rgb'
+INPUT_SHAPE = (512, 512, 1)
 SALIENCY_INPUT_SHAPE = (224, 224, 3)
 BATCH_SIZE = 1
 NUM_EPOCHS = 20
